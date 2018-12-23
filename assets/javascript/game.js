345c3519caf1ec71fd917ade7b63da0895fd8c06
Aaron.Gauntt <gauntta@gmail.com>
2018-12-22 23:18:25 -0600

    EOD 12.22

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,27 @@
+
+
 function Genie() {
 var alphabet = "abcedfghijklmnopqrstuvwxyz";
 var randomLetter = alphabet[Math.floor(Math.random() * alphabet.length)];
+var guess = document.getElementById("guesses");
 console.log(randomLetter);
+console.log(guess);
+
+if (randomLetter === guess) {
+    console.log("you Win!");  
+  } else {
+    console.log("Not that one!")  
+  }
 };
 
 
-window.onkeypress = function (evt) {
+/*window.onkeypress = function (evt) {
     evt = evt || window.event;
     var keyCode = evt.keyCode || evt.which;
     var guess = String.fromCharCode(keyCode);
     console.log(guess);
-}
+}*/
+
+
    
+
